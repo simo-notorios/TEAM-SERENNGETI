975eebb1804e43471034979c1bb598ac3116ad93
simo-notorios <simokfc846@gmail.com>
2025-12-08 18:43:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Fade-in عند تحميل الصفحات
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.style.opacity = 0;
+    setTimeout(() => {
+        document.body.style.transition = "opacity 1s";
+        document.body.style.opacity = 1;
+    }, 100);
+});
